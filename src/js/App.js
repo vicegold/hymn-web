0a24b3d12530a3c7965456aee25aed79f7b3e259
laurids <laurids@vicegold.com>
2017-07-10 13:31:35 +0200

    add all contect sections

--- src/js/App.js
+++ src/js/App.js
@@ -21,3 +21,39 @@
 //     })
 //     .setPin("#my-sticky-element") // pins the element for the the scene's duration
 //     .addTo(controller); // assign the scene to the controller
+
+
+import $ from "jquery";
+import slick from "slick-carousel";
+import smoothScroll from "smooth-scroll";
+
+$(".hymn-slider").slick({
+
+  // the magic
+  responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+
+    }, {
+
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+
+    }]
+});
+
+smoothScroll.init({
+  speed: 1000 // Integer. How fast to complete the scroll in milliseconds
+});
